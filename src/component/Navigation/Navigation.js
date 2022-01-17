c9435a9189d88de8a6378f02812ef896444d16e9
S M Mahsanul Islam Nirjhor <mahsanulnirjhor@gmail.com>
2022-01-17 11:17:11 +0600

    Navigation component is added

---
+++ src/component/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// Pure function
+
+const Navigation = () => {
+    return (
+        <nav>
+            <p>Sign Out</p>
+        </nav>
+    );
+
+}
+export default Navigation;
